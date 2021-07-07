f02e7b5c8336572aa60d29146f98a27666c4933d
adx <nogo22@naver.com>
2021-07-08 05:29:12 +0900

    폼 입력처리

--- client/src/pages/WritePage/index.js
+++ client/src/pages/WritePage/index.js
@@ -4,21 +4,49 @@ import axios from "axios";
 
 function WritePage() {
   //글쓰기 함수 처리해줘야함 + 서버로 요청 보내서 db에 삽입하기
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  // const [File, setFile] = useState("")
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const onSubmit = () => {
+    const data = {
+      title: title,
+      description: description,
+    };
+
+    console.log(data);
+  };
+
+  // const
 
   return (
     <Container>
+      {/* <form action=""></form> */}
       <h3>글쓰기</h3>
       <label For="title">제목</label>
-      <input type="text" id="title" />
+      <input type="text" id="title" value={title} onChange={onChangeTitle} />
       <label For="description">내용</label>
-      <input type="text" id="description" />
+      <input
+        type="text"
+        id="description"
+        value={description}
+        onChange={onChangeDescription}
+      />
       <div id="file">
         <label For="title">첨부파일</label>
         <input type="text" id="file" />
         <button>추가</button>
         <button>삭제</button>
       </div>
-      <button>등록</button>
+      <button onClick={onSubmit}>등록</button>
     </Container>
   );
 }
