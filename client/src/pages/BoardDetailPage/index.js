1361c9a4a050bf1dfaa7cb6e7c2ea0c060514a1a
adu <nogo22@naver.com>
2021-07-10 01:38:15 +0900

    수정하기 구현중 기존의 값이 유지되지 않는 문제발생

--- client/src/pages/BoardDetailPage/index.js
+++ client/src/pages/BoardDetailPage/index.js
@@ -4,18 +4,13 @@ import { Container, Lodding } from "./styled";
 import axios from "axios";
 
 function BoardDetailPage() {
-  const [detailPost, setDetailPost] = useState("");
-  const { pathname } = useLocation();
-  const postId = pathname.substring(12);
-  const args = {
-    _id: postId,
-  };
-
   useEffect(() => {
     axios
       .post("http://localhost:5050/api/board/detail", args)
       .then(({ data }) => {
         setDetailPost(data);
+        setTitle(data.title);
+        setDescription(data.description);
         console.log("detail data :: ", data);
       })
       .catch((err) => {
@@ -23,19 +18,67 @@ function BoardDetailPage() {
       });
   }, []);
 
+  const [detailPost, setDetailPost] = useState("");
+  const { pathname } = useLocation();
+  const postId = pathname.substring(12);
+  const args = {
+    _id: postId,
+  };
+
+  const [upadteMode, setUpadteMode] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onClickPostUpdate = () => {
+    setUpadteMode(true);
+  };
+
+  const onClickPostUpdateComplete = () => {
+    setUpadteMode(false);
+  };
+
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
   if (!detailPost) {
     return <Lodding>로딩중</Lodding>;
   } else {
-    return (
-      <Container>
-        {detailPost && <button>수정하기</button>}
-        <br />
-        <Link to="/menu/board">뒤로가기</Link>
-        <h3>{detailPost.data.title}</h3>
+    if (!upadteMode) {
+      return (
+        <Container>
+          {detailPost && <button onClick={onClickPostUpdate}>수정하기</button>}
+          <br />
+          <Link to="/menu/board">뒤로가기</Link>
+          <h3>{detailPost.data.title}</h3>
 
-        <p>{detailPost.data.description}</p>
-      </Container>
-    );
+          <p>{detailPost.data.description}</p>
+        </Container>
+      );
+    } else {
+      return (
+        <Container>
+          {detailPost && (
+            <button onClick={onClickPostUpdateComplete}>수정완료</button>
+          )}
+          <br />
+          <Link to="/menu/board">뒤로가기</Link>
+          <h3>{detailPost.data.title}</h3>
+          <input type="text" value={title} onChange={onChangeTitle} />
+          <input
+            type="text"
+            value={description}
+            onChange={onChangeDescription}
+          />
+
+          <p>{detailPost.data.description}</p>
+        </Container>
+      );
+    }
   }
 }
 
--- server/routes/board.js
+++ server/routes/board.js
@@ -4,6 +4,39 @@ const router = express.Router();
 
 const { Board } = require("../models/Board");
 
+router.post("/delete", (req, res) => {
+  const board = new Board(req.body);
+  console.log("board :: ", board);
+
+  board.save((err, data) => {
+    if (err) return res.json({ success: false, err });
+    console.log("save data: ", data);
+    return res.status(200).json({
+      signUpSuccess: true,
+      message: "게시물 업로드 성공",
+    });
+  });
+});
+
+router.post("/update", (req, res) => {
+  const target = req.body;
+  console.log("target :: ", target);
+
+  Board.update({ _id: target.id }, { $set: { target } })
+    .then((board) => {
+      console.log("DB에서 게시글 불러오기 성공");
+      return res.status(200).json({
+        board,
+      });
+      res.json({ message: "게시글을 성공적으로 불러왔습니다." });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err,
+      });
+    });
+});
+
 router.get("/getall", (req, res) => {
   Board.find()
     .then((board) => {
