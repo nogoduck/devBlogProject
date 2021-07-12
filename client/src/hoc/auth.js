0a2905c2de89bfa1eb40ff38ddb2e53dba7cec9e
adu <nogo22@naver.com>
2021-07-13 04:57:22 +0900

    로그인 안된 상태에서의 접근제한 페이지 구현 실패

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,24 +1,40 @@
+import { withRouter } from "react-router-dom";
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { auth } from "../_actions/user_actions";
-import axios from "axios";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (ArgsComponent, option, admin = null) {
-  function AuthenticationCheck(props) {
+  function AuthenticationCheck({ history }) {
     const dispatch = useDispatch();
 
     useEffect(() => {
       dispatch(auth()).then((res) => {
-        console.log("auth::: ", res);
+        console.log("hoc - auth: ", res);
 
         if (res.payload.isAuth) {
+          console.log("Login State => Succeed");
+          //로그인이 된 상황
           if (option) {
-            console.log("페이지 접근 금지 처리");
-            //props.history 사용예정
+            //optione === true
+            //로그인이 된 상태에서 출입 불가능한 페이지
+            history.push("/");
+          } else {
+            //optione === false
+            // history.push("/");
           }
         } else {
+          console.log("Login State => null");
+          //로그인이 안된 상황
+          if (option) {
+            //optione === true
+            //로그인이 안된 상태에서 출입 불가능한 페이지
+            // history.push("/");
+          } else {
+            //optione === false
+            // history.push("/");
+          }
         }
       });
     }, []);
@@ -26,5 +42,5 @@ export default function (ArgsComponent, option, admin = null) {
     return <ArgsComponent />;
   }
 
-  return AuthenticationCheck;
+  return withRouter(AuthenticationCheck);
 }
--- client/src/pages/LandingPage/index.js
+++ client/src/pages/LandingPage/index.js
@@ -17,6 +17,14 @@ import BoardDetailPage from "../BoardDetailPage";
 
 import Auth from "../../hoc/auth";
 
+// Auth 매개변수:
+// (1, 2, 3) :
+// 1: 컴포넌트
+// 2: null(로그인 여부 상관 없음),
+//   true(로그인 한 유저만 랜더링),
+//   false(로그인 안한 유저는 접근제한)
+// 3: 값을 넘기지 않으면 기본값 null, 유저등급 지정
+// ex) 0 = 1 = admin, 2 = guest
 function LandingPage() {
   return (
     <>
@@ -29,14 +37,14 @@ function LandingPage() {
             <Route path="/menu/about" component={Auth(AboutPage, null)} />
             <Route
               path="/menu/board/write"
-              component={Auth(BoardWritePage, null)}
+              component={Auth(BoardWritePage, true)}
             />
             <Route
               path="/menu/board/:postId"
               component={Auth(BoardDetailPage, null)}
             />
-            <Route path="/menu/board" component={Auth(BoardPage, false)} />
-            <Route path="/menu/card" component={Auth(CardPage, true)} />
+            <Route path="/menu/board" component={Auth(BoardPage, null)} />
+            <Route path="/menu/card" component={Auth(CardPage, false)} />
             <Route path="/menu/modal" component={Auth(ModalPage, true)} />
             <Route path="/menu/video" component={Auth(VideoPage, null)} />
           </Switch>
