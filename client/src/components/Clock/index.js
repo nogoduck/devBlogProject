6a3f4cf9b2188b89f31242a93bf1fb8620d14507
adu <nogo22@naver.com>
2021-07-19 03:02:43 +0900

    현재시각 메뉴에 구현

--- client/src/components/Clock/index.js
+++ client/src/components/Clock/index.js
@@ -3,14 +3,13 @@ import { Container } from "./styled";
 import { getClock } from "./func";
 function Clock() {
   console.log("clock >> ", getClock());
-  const [currentTime, setCurrentTime] = useState("00 : 00 : 00");
+  const [currentTime, setCurrentTime] = useState(getClock);
 
   useEffect(() => {
-    setCurrentTime(setInterval(getClock, 1000));
-  });
-  // useEffect(() => {
-  //   setCurrentTime(setInterval(getClock, 1000));
-  // }, []);
+    setInterval(() => {
+      setCurrentTime(getClock);
+    }, 1000);
+  }, [currentTime]);
 
   return <Container>{currentTime}</Container>;
 }
