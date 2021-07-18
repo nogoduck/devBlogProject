dda87a497abd480e231a9a537694341a388203f5
adx <nogo22@naver.com>
2021-07-18 13:38:21 +0900

    현재시간 함수 미완성

--- client/src/components/Clock/index.js
+++ client/src/components/Clock/index.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from "./styled";
-
+import { getClock } from "./func";
 function Clock() {
-  return <Container>공용 시계</Container>;
+  console.log("clock >> ", getClock());
+  const [currentTime, setCurrentTime] = useState("00 : 00 : 00");
+
+  useEffect(() => {
+    setCurrentTime(setInterval(getClock, 1000));
+  });
+  // useEffect(() => {
+  //   setCurrentTime(setInterval(getClock, 1000));
+  // }, []);
+
+  return <Container>{currentTime}</Container>;
 }
 
 export default Clock;
