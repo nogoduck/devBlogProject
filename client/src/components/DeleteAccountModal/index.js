a11764aee39b49f1b175786868af6159f20d8d8a
adx <nogo22@naver.com>
2021-07-21 10:55:15 +0900

    회원탈퇴 모달 디자인 완료

--- client/src/components/DeleteAccountModal/index.js
+++ client/src/components/DeleteAccountModal/index.js
@@ -1,37 +1,31 @@
 import React from "react";
-import {
-  DeleteAccountModalCancelButton,
-  DeleteAccountModalSubmitButton,
-} from "./styled";
+import { Input, Label, Error, DeleteAccountForm } from "./styled";
+import axios from "axios";
 
 import AlertModal from "../AlertModal";
 
 function DeleteAccountModal({ show, close }) {
+  const onSubmitDeleteAccount = () => {
+    console.log("delete user");
+  };
+
   return (
     <AlertModal
-      show={true}
-      // close={}
+      show={show}
+      close={close}
       modalHeader="회원탈퇴 주의사항"
       notice="계정을 탈퇴하면 되돌릴 수 없습니다. 신중히 생각해주세요"
       content="아래 진행사항을 입력해주세요"
       option="danger"
+      confirm={onSubmitDeleteAccount}
     >
-      <label htmlFor="check-email">이메일</label>
-      <input type="text" id="check-email" />
-      <br />
-      <label htmlFor="check-email">비밀번호</label>
-      <input type="text" id="check-email" />
-
-      <DeleteAccountModalCancelButton
-      // onClick={ }
-      >
-        취소
-      </DeleteAccountModalCancelButton>
-      <DeleteAccountModalSubmitButton
-      // onClick={}
-      >
-        확인
-      </DeleteAccountModalSubmitButton>
+      <hr />
+      <DeleteAccountForm>
+        <Label htmlFor="check-email">현재 로그인된 계정 이메일</Label>
+        <Input type="text" id="check-email" />
+        <Label htmlFor="check-email">현재 로그인된 계정 비밀번호</Label>
+        <Input type="text" id="check-email" />
+      </DeleteAccountForm>
     </AlertModal>
   );
 }
