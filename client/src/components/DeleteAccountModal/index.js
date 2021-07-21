b2c9a09d8e939c93de68b00eb8c5808b5c5cb343
adx <nogo22@naver.com>
2021-07-21 15:37:35 +0900

    회원탈퇴 모달, 기능 구현 완료

--- client/src/components/DeleteAccountModal/index.js
+++ client/src/components/DeleteAccountModal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Input, Label, Error, DeleteAccountForm } from "./styled";
@@ -7,16 +7,18 @@ import axios from "axios";
 import useInput from "../../hooks/useInput";
 import AlertModal from "../AlertModal";
 
-function DeleteAccountModal({ show, close }) {
+function DeleteAccountModal({ history, show, close }) {
   const user = useSelector((state) => state.user);
 
   const [email, setEmail, onChangeEmail] = useInput();
   const [password, setPassword, onChangePassword] = useInput();
-  console.log("v >> ", email, password);
+  const [showSuccessDeleteAccountModal, setShowSuccessDeleteAccountModal] =
+    useState(false);
+
+  // console.log("show > ", show);
+  // console.log("close > ", close);
 
   const onSubmitDeleteAccount = () => {
-    console.log(email, password);
-    console.log(user.authStatus._id);
     const payload = {
       _id: user.authStatus._id,
       email: email,
@@ -31,41 +33,63 @@ function DeleteAccountModal({ show, close }) {
         withCredentials: true,
       })
       .then(({ data }) => {
-        console.log("Delete Post data >> ", data);
+        //회원탈퇴 성공
+        if (data.deleteAccount) {
+          console.log("Delete Account Succeess >> ", data);
+          close();
+          setShowSuccessDeleteAccountModal(true);
+        }
       })
       .catch((err) => {
-        console.log("Delete Post Error >> ", err);
+        console.log("Delete Account Error >> ", err);
       });
   };
 
+  const onSubmitSuccessDeleteAccount = () => {
+    setShowSuccessDeleteAccountModal(false);
+    history.push("/");
+  };
+
   return (
-    <AlertModal
-      show={show}
-      close={close}
-      modalHeader="회원탈퇴 주의사항"
-      notice="계정을 탈퇴하면 되돌릴 수 없습니다. 신중히 생각해주세요"
-      content="아래 진행사항을 입력해주세요"
-      option="danger"
-      confirm={onSubmitDeleteAccount}
-    >
-      <hr />
-      <DeleteAccountForm>
-        <Label htmlFor="check-email">현재 로그인된 계정 이메일</Label>
-        <Input
-          type="text"
-          id="check-email"
-          value={email}
-          onChange={onChangeEmail}
-        />
-        <Label htmlFor="check-email">현재 로그인된 계정 비밀번호</Label>
-        <Input
-          type="password"
-          id="check-email"
-          value={password}
-          onChange={onChangePassword}
-        />
-      </DeleteAccountForm>
-    </AlertModal>
+    <div>
+      <AlertModal
+        show={show}
+        close={close}
+        modalHeader="회원탈퇴 주의사항"
+        notice="계정을 탈퇴하면 되돌릴 수 없습니다. 신중히 생각해주세요"
+        content="아래 진행사항을 입력해주세요"
+        option="danger"
+        confirm={onSubmitDeleteAccount}
+      >
+        <hr />
+        <DeleteAccountForm>
+          <Label htmlFor="check-email">현재 로그인된 계정 이메일</Label>
+          <Input
+            type="text"
+            id="check-email"
+            value={email}
+            onChange={onChangeEmail}
+          />
+          <Label htmlFor="check-email">현재 로그인된 계정 비밀번호</Label>
+          <Input
+            type="password"
+            id="check-email"
+            value={password}
+            onChange={onChangePassword}
+          />
+        </DeleteAccountForm>
+      </AlertModal>
+      <AlertModal
+        show={showSuccessDeleteAccountModal}
+        close={onSubmitSuccessDeleteAccount}
+        modalHeader="회원탈퇴"
+        title="회원탈퇴에 성공했습니다"
+        content="이용해주셔서 감사합니다"
+        option="success"
+        useCancelButton={false}
+        confirm={onSubmitSuccessDeleteAccount}
+      ></AlertModal>
+    </div>
   );
 }
 
