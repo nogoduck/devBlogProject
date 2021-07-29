e53b7cd50cd5ae3ca51f2eedaf4a3d36c92c968f
adx <nogo22@naver.com>
2021-07-30 00:30:53 +0900

    Thumbnail creation complete, structured the value that came in at the time of the request, and put it in the ffmpeg parameter, so it's solved.

--- server/routes/video.js
+++ server/routes/video.js
@@ -59,11 +59,11 @@ router.post("/getVideoDetail", (req, res) => {
 
 
 router.post("/createVideo", (req, res) => {
+//비디오 파일을 올릴때 썸네일 생성전 비디오를 저장하는 라우터
     upload(req, res, (err) => {
         if (err) {
             return res.json({success: false, err});
         }
-        console.log("req.file >> ", req.file);
         const { path, filename } = req.file;
         return res.json({
             success: true,
@@ -74,17 +74,17 @@ router.post("/createVideo", (req, res) => {
 });
 
 router.post("/thumbnail", (req, res) => {
-    const {filePath, videoLength} = req.body;
+// 비디오를 저장 후에 썸네일을 생성하는 라우터
     console.log('req.body Thumbnail >> ', req.body);
+    //ffmpeg 압축 푼 경로
     ffmpeg.setFfmpegPath("C:\\ffmpeg-4.4-full_build\\bin\\ffmpeg.exe");
-
-    ffmpeg.ffprobe(req.body.url, (err, metadata) => {
+    let filePath, videoLength
+    ffmpeg.ffprobe(req.body.filePath, (err, metadata) => {
         console.dir('metadata > ', metadata);
         console.log('dr > ', metadata.format.duration);
         videoLength = metadata.format.duration;
     });
-
-    ffmpeg(req.body.url)
+    ffmpeg(req.body.filePath)
         .on("filenames", (filenames) => {
             console.log("Will generate ", filenames.join(", "));
             console.log(filenames);
