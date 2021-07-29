f061402511161435b2a3b94461e0c8e5370aeef6
adx <nogo22@naver.com>
2021-07-29 13:24:28 +0900

    mongoose connect config -> ENV

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use("/zDummyFile", express.static("zDummyFile"));
 //=====================================
 
 mongoose
-  .connect(config.mongoURI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
