1bdf7096833172ecf5d9294ac4f003c2f671f6d7
adx <nogo22@naver.com>
2021-07-30 02:09:30 +0900

    Prettier applied successfully, ESLint failed

--- client/src/pages/VideoUploadPage/index.js
+++ client/src/pages/VideoUploadPage/index.js
@@ -1,98 +1,103 @@
-import {withRouter} from "react-router-dom";
-import React, {useEffect, useState} from "react";
-import axios from "axios";
-import Dropzone from "react-dropzone";
-import {useSelector} from "react-redux";
+import { withRouter } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Dropzone from 'react-dropzone';
+import { useSelector } from 'react-redux';
+import useInput from '../../hooks/useInput';
 
+function VideoUploadPage({ history }) {
+  const user = useSelector((state) => state.user);
+  const [videoTitle, setVideoTitle, onChangeVideoTitle] = useInput();
+  const [videoDescription, setVideoDescription, onChangeVideoDescription] =
+    useInput();
+  const [videoPath, setVideoPath] = useState('');
+  const [videoLength, setVideoLength] = useState('');
+  const [thumbnailPath, setThumbnailPath] = useState('');
 
-function VideoUploadPage({history}) {
-    const user = useSelector((state) => state.user);
-    const [videoTitle, setVideoTitle] = useState("");
-    const [videoDescription, setVideoDescription] = useState("");
-    const [videoPath, setVideoPath] = useState("");
-    const [videoLength, setVideoLength] = useState("");
-    const [thumbnailPath, setThumbnailPath] = useState("");
-
-
-    const [video, setVideo] = useState([])
-
-    const onDropVideo = (files) => {
-        let formData = new FormData();
-        console.log(files);
-        const config = {
-            header: {"content-type": "multipart/form-data"},
-        };
-        formData.append("file", files[0]);
-
-        axios.post("/api/video/createVideo", formData, config).then(({data}) => {
-            console.log('data createVideo >>', data);
-            if (data.success) {
-                setVideoPath(data.filePath);
-                let paylaod = {
-                    filePath: data.filePath,
-                    fileName: data.fileName,
-                };
-
-                axios.post("/api/video/thumbnail", paylaod).then(({data}) => {
-                    console.log('data thumbnail >>', data);
-                    if (data.success) {
-                        setVideoLength(data.videoLength);
-                        setThumbnailPath(data.thumbnailPath);
-                    } else {
-                        alert("썸네일 생성을 실패했습니다.");
-                    }
-                });
-            } else {
-                alert("비디오 생성에 실패했습니다");
-            }
-        });
-    }
-
-    const onSubmitUploadVideo = (e) => {
-        e.preventDefault();
-
-        const payload = {
-            writer: user.authStatus._id,
-            title: videoTitle,
-            description: videoDescription,
-            videoPath: videoPath,
-            videoLength: videoLength,
-            thumbnailPath: thumbnailPath,
-        };
-
-        axios.post("/api/video/uploadVideo", payload).then(({data}) => {
-            console.log("data >> ", data);
-            if (data.success) {
-                alert("성공적으로 업로드를 했습니다.");
-                setTimeout(() => {
-                    history.push("/menu/video");
-                }, 0);
-            } else {
-                alert("비디오 업로드에 실패했습니다.");
-            }
-        }).catch((err) => {
-            alert("비디오 업로드에 실패했습니다.");
-        })
+  const onDropVideo = (files) => {
+    let formData = new FormData();
+    console.log(files);
+    const config = {
+      header: { 'content-type': 'multipart/form-data' },
     };
 
-    return <>
-        <form onSubmit={onSubmitUploadVideo}>
-            <Dropzone onDrop={onDropVideo} multiple={false} maxSize={10_0000_0000}>
-                {({getRootProps, getInputProps}) => (
-                    <div className="dropzone_content" {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <div className="ico_drop">영상 파일을 드래그하거나 선택해주세요.</div>
-                    </div>
-                )}
-            </Dropzone>
-            {thumbnailPath && (
-                <img
-                    src={`http://localhost:5050/${thumbnailPath}`}
-                />
-            )}
-            <button type="submit">~비디오 등록 !</button>
-        </form>
-    </>;
+    formData.append('file', files[0]);
+
+    axios.post('/api/video/createVideo', formData, config).then(({ data }) => {
+      console.log('data createVideo >>', data);
+      if (data.success) {
+        setVideoPath(data.filePath);
+        let paylaod = {
+          filePath: data.filePath,
+          fileName: data.fileName,
+        };
+
+        axios.post('/api/video/thumbnail', paylaod).then(({ data }) => {
+          console.log('data thumbnail >>', data);
+          if (data.success) {
+            setVideoLength(data.videoLength);
+            setThumbnailPath(data.thumbnailPath);
+          } else {
+            alert('썸네일 생성을 실패했습니다.');
+          }
+        });
+      } else {
+        alert('비디오 생성에 실패했습니다');
+      }
+    });
+  };
+
+  const onSubmitUploadVideo = (e) => {
+    e.preventDefault();
+
+    const payload = {
+      publisher: user.authStatus._id,
+      title: videoTitle,
+      description: videoDescription,
+      videoPath: videoPath,
+      videoLength: videoLength,
+      thumbnailPath: thumbnailPath,
+    };
+
+    axios
+      .post('/api/video/uploadVideo', payload)
+      .then(({ data }) => {
+        console.log('data >> ', data);
+        if (data.success) {
+          alert('성공적으로 업로드를 했습니다.');
+          setTimeout(() => {
+            history.push('/menu/video');
+          }, 0);
+        } else {
+          alert('비디오 업로드에 실패했습니다');
+        }
+      })
+      .catch((err) => {
+        alert('비디오 업로드에 실패했습니다.');
+      });
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmitUploadVideo}>
+        <Dropzone onDrop={onDropVideo} multiple={false} maxSize={10_0000_0000}>
+          {({ getRootProps, getInputProps }) => (
+            <div className="dropzone_content" {...getRootProps()}>
+              <input {...getInputProps()} />
+              <div className="ico_drop">
+                영상 파일을 드래그하거나 선택해주세요.
+              </div>
+            </div>
+          )}
+        </Dropzone>
+        {thumbnailPath && (
+          <img src={`http://localhost:5050/${thumbnailPath}`} />
+        )}
+        <input type="text" value={videoTitle} />
+        <button type="submit">~비디오 등록 !</button>
+      </form>
+    </>
+  );
 }
 
 export default withRouter(VideoUploadPage);
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,108 +1,122 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const multer = require("multer");
-const ffmpeg = require("fluent-ffmpeg");
-const { Video } = require("../models/Video");
+const multer = require('multer');
+const ffmpeg = require('fluent-ffmpeg');
+const { Video } = require('../models/Video');
 
 let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "UploadVideo/");
-        console.log(file);
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}_${file.originalname}`);
-    },
-    fileFilter: (req, file, cb) => {
-        const ext = path.extname(file.originalname);
-        if (ext !== ".mp4") {
-            return cb(res.status(400).end("only mp4 is allowed"), false);
-        }
-        cb(null, true);
-    },
+  destination: (req, file, cb) => {
+    cb(null, 'UploadVideo/');
+    console.log(file);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.mp4') {
+      return cb(res.status(400).end('only mp4 is allowed'), false);
+    }
+    cb(null, true);
+  },
 });
 
-const upload = multer({storage: storage}).single("file");
+const upload = multer({ storage: storage }).single('file');
 
-
-router.get("/getAll", (req, res) => {
-    Video.find()
-        .populate("publisher")
-        .exec((err, videos) => {
-            if (err) return res.status(400).send(err);
-            res.status(200).json({success: true, videos});
-        });
+router.get('/getAll', (req, res) => {
+  Video.find()
+    .populate('publisher')
+    .exec((err, videos) => {
+      if (err) return res.status(400).send(err);
+      res.status(200).json({ success: true, videos });
+    });
 });
 
-router.post("/getVideoDetail", (req, res) => {
-    console.log("req.body : ", req.body.videoId);
-    Video.findOne({_id: req.body.videoId})
-        .populate("writer")
-        .exec((err, videoDetail) => {
-            if (err) return res.status(400).send(err);
-            console.log("videoDetail :: ", videoDetail);
-            res.status(200).json({success: true, videoDetail});
-        });
+router.post('/getVideoDetail', (req, res) => {
+  console.log('req.body : ', req.body.videoId);
+  Video.findOne({ _id: req.body.videoId })
+
+    .populate('writer')
+    .exec((err, videoDetail) => {
+      if (err) return res.status(400).send(err);
+      console.log('videoDetail :: ', videoDetail);
+      res.status(200).json({ success: true, videoDetail });
+    });
 });
 
-router.post("/uploadVideo", (req, res) => {
-    const video = new Video(req.body);
-    console.log("video >> ", video);
+router.post('/uploadVideo', (req, res) => {
+  const video = new Video(req.body);
+  console.log('video >> ', video);
+
+  //예외처리 ( 빈값 들어오면 반환 ), 제목만 빈 값 허용안하고 내용은 허용함
+  if (
+    video.publisher === '' ||
+    video.videoPath === '' ||
+    video.thumbnailPath === '' ||
+    video.title === ''
+  ) {
+    res.status(400).json({
+      success: false,
+      message: '유저정보 또는 제목이 잘못되었습니다.',
+    });
+  } else {
     video.save((err, doc) => {
-        if (err) return res.json({success: false, err});
-        console.log("upload save doc >> ", doc);
-        res.status(200).json({success: true});
+      if (err) return res.json({ success: false, err });
+      console.log('upload save doc >> ', doc);
+      res.status(200).json({ success: true });
     });
+  }
 });
 
-router.post("/createVideo", (req, res) => {
-//비디오 파일을 올릴때 썸네일 생성전 비디오를 저장하는 라우터
-    upload(req, res, (err) => {
-        if (err) {
-            return res.json({success: false, err});
-        }
-        const { path, filename } = req.file;
-        return res.json({
-            success: true,
-            filePath: path,
-            fileName: filename
-        });
+router.post('/createVideo', (req, res) => {
+  //비디오 파일을 올릴때 썸네일 생성전 비디오를 저장하는 라우터
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    const { path, filename } = req.file;
+    return res.json({
+      success: true,
+      filePath: path,
+      fileName: filename,
     });
+  });
 });
 
-router.post("/thumbnail", (req, res) => {
-// 비디오를 저장 후에 썸네일을 생성하는 라우터
-    console.log('req.body Thumbnail >> ', req.body);
-    //ffmpeg 압축 푼 경로
-    ffmpeg.setFfmpegPath("C:\\ffmpeg-4.4-full_build\\bin\\ffmpeg.exe");
-    let thumbnailPath, videoLength
-    ffmpeg.ffprobe(req.body.filePath, (err, metadata) => {
-        console.dir('metadata > ', metadata);
-        console.log('dr > ', metadata.format.duration);
-        videoLength = metadata.format.duration;
+router.post('/thumbnail', (req, res) => {
+  // 비디오를 저장 후에 썸네일을 생성하는 라우터
+  console.log('req.body Thumbnail >> ', req.body);
+  //ffmpeg 압축 푼 경로
+  ffmpeg.setFfmpegPath('C:\\ffmpeg-4.4-full_build\\bin\\ffmpeg.exe');
+  let thumbnailPath, videoLength;
+  ffmpeg.ffprobe(req.body.filePath, (err, metadata) => {
+    console.dir('metadata > ', metadata);
+    console.log('dr > ', metadata.format.duration);
+    videoLength = metadata.format.duration;
+  });
+  ffmpeg(req.body.filePath)
+    .on('filenames', (filenames) => {
+      console.log('filenames >> ', filenames);
+      thumbnailPath = 'UploadVideo/thumbnail/' + filenames[0];
+    })
+    .on('end', () => {
+      console.log('Succeess Create thumbnail');
+      return res.json({
+        success: true,
+        thumbnailPath: thumbnailPath,
+        videoLength: videoLength,
+      });
+    })
+    .on('error', (err) => {
+      console.log(err);
+      return res.json({ success: false, err });
+    })
+    .screenshots({
+      count: 3,
+      folder: 'UploadVideo/thumbnail',
+      size: '300x180',
+      filename: 'thumbnail-%b.png',
     });
-    ffmpeg(req.body.filePath)
-        .on("filenames", (filenames) => {
-            console.log("filenames >> ", filenames);
-            thumbnailPath = "UploadVideo/thumbnail/" + filenames[0];
-        })
-        .on("end", () => {
-            console.log("Succeess Create thumbnail");
-            return res.json({
-                success: true,
-                thumbnailPath: thumbnailPath,
-                videoLength: videoLength,
-            });
-        })
-        .on("error", (err) => {
-            console.log(err);
-            return res.json({success: false, err});
-        })
-        .screenshots({
-            count: 3,
-            folder: "UploadVideo/thumbnail",
-            size: "300x180",
-            filename: "thumbnail-%b.png",
-        });
 });
 
 module.exports = router;
