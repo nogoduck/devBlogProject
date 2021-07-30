733db67ee2d65a6724faecd9ef369ebd0a481a29
adx <nogo22@naver.com>
2021-07-30 12:08:26 +0900

    Successful Import of Registered Videos

--- client/src/pages/VideoPage/index.js
+++ client/src/pages/VideoPage/index.js
@@ -1,11 +1,16 @@
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import VideoUploadPage from '../VideoUploadPage';
 import Static from '../../setupStatic';
+import moment from 'moment';
+import conversionSeconds from '../../_utils/conversionSeconds';
+import { useSelector } from 'react-redux';
+import Gravatar from 'react-gravatar';
 
 function VideoPage() {
+  const user = useSelector((state) => state.user);
   const [videoList, setVideoList] = useState([]);
 
   useEffect(() => {
@@ -24,10 +29,56 @@ function VideoPage() {
       });
   }, []);
 
+  const videoCards = videoList.map((v, i) => {
+    let momentVideoLength = moment
+      .utc(1000 * v.videoLength)
+      .format('H[h] mm[m] ss[s]');
+    let momentCreatedAt = moment(v.createdAt).format('YY MMM Do ');
+    console.log('videoPlayTime >> ', momentVideoLength);
+    console.log('momentCreatedAt >> ', momentCreatedAt);
+
+    return (
+      <Link to={`/menu/video/${v._id}`} className="video__container">
+        <img src={`${Static.URI}${v.thumbnailPath}`} alt="thumbnail_IMG" />
+        <div className="duration__absolute">
+          <div className="duration__relative">
+            <span>{momentVideoLength}</span>
+          </div>
+        </div>
+        <div className="video__info">
+          <div className="title">
+            <span className="profile__img">
+              {v.publisher.imagePath ? (
+                <img
+                  src={`${Static.URI}${v.publisher.imagePath}`}
+                  alt="profile_image"
+                />
+              ) : (
+                <Gravatar
+                  email={v.publisher.email}
+                  size={250}
+                  default="wavatar"
+                />
+              )}
+            </span>
+            <span className=" video__title">{v.title}</span>
+          </div>
+          <div className="video__article video__writerName">
+            {v.publisher.name}
+          </div>
+          <div className="video__article video__date">
+            {v.view} · {momentCreatedAt}
+          </div>
+        </div>
+      </Link>
+    );
+  });
+
   return (
     <div>
       Movie, drama, docu <br />
       <div>{Static.URI}</div>
+      {videoCards}
       <VideoUploadPage></VideoUploadPage>
     </div>
   );
--- client/src/setupStatic.js
+++ client/src/setupStatic.js
@@ -1,5 +1,7 @@
-const staticURI = process.env.STATIC_URI || 'http://localhost:5050/';
-
+const staticURI =
+  process.env.NODE_ENV === 'production'
+    ? 'https://devlog-ad.herokuapp.com'
+    : 'http://localhost:5050/';
 module.exports = {
   URI: staticURI,
 };
