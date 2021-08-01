330a7342cc144656166f4f3b23c1bc91771ae8ec
adu <nogo22@naver.com>
2021-08-02 00:17:23 +0900

    deploy push

--- server/routes/zTest.js
+++ server/routes/zTest.js
@@ -88,7 +88,11 @@ router.post('/form/save', (req, res) => {
 
 router.get('/', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
   res.send('response testRouter...');
 });
 
