238e39857b2798f6943cd07047501adc2bc72e43
adu <nogo22@naver.com>
2021-08-03 05:25:03 +0900

    쿠키 도메인 변경

--- server/routes/zTest.js
+++ server/routes/zTest.js
@@ -101,33 +101,37 @@ router.get('/cookie1', (req, res) => {
   res.cookie('Purple2Cookie', 'eggplant', {
     secure: true,
     httpOnly: true,
-    domain: '.adklog.com/',
+    domain: 'adklog.com/',
   });
   res.cookie('Red2Cookie', 'apple', {
     sameSite: 'lax',
-    domain: '.adklog.com/',
+    domain: 'adklog.com/',
   });
   res.cookie('DotRed2Cookie', 'a3p4ple', {
     sameSite: 'none',
     secure: true,
-    domain: '.adklog.com/',
+    domain: 'adklog.com/',
   });
   res.cookie('XX1Red2Cookie', '93apple', {
     sameSite: 'none',
     secure: true,
-    domain: '.adklog.com/',
+    domain: 'adklog.com/',
   });
   res.cookie('None2Cookie', 'NONE', {
     sameSite: 'none',
     secure: true,
-    domain: '.adklog.com/',
+    domain: 'adklog.com/',
   });
   res.cookie('PurpleCookie', 'grape').send('쿠키 생성에 성공했습니다.');
 });
 
 router.post('/cookie2', (req, res) => {
   res
-    .cookie('YellowCookie', 'banana', { secure: true, httpOnly: true })
+    .cookie('YellowCookie', 'banana', {
+      sameSite: 'none',
+      secure: true,
+      httpOnly: true,
+    })
     .send('쿠키 생성2에(Post) 성공했습니다.');
 });
 
